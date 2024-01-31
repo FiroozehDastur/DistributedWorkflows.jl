15da3eba482b47d96f2e868ca0fd7b0116cf3ea3
root <firoozehaga@gmail.com>
2024-01-31 01:15:53 +0000

    testing workflow example

--- src/DistributedWorkflow.jl
+++ src/DistributedWorkflow.jl
@@ -1,6 +1,7 @@
 module DistributedWorkflow
   using CxxWrap, Serialization
-  export compile_workflow, client, function_name, implementation, initiate_connection, input_pair, julia_implementation, output_dir, port_info, submit_workflow, value_info
+  import .CxxWrap: StdVector
+  export compile_workflow, client_init, function_name, implementation, initiate_connection, input_pair, julia_implementation, output_dir, port_info, submit_workflow, value_info, Workflow
 
   include("cxxwrap_calls.jl")
   include("workflow_compiler.jl")
--- src/wrapper.jl
+++ src/wrapper.jl
@@ -1,6 +1,6 @@
-include("./cxxwrap_calls.jl")
+# include("./cxxwrap_calls.jl")
 
-function client(workers::Int, nodefile::String, rif_strategy::String, log_host::String, log_port::Int)
+function client_init(workers::Int, nodefile::String, rif_strategy::String, log_host::String, log_port::Int)
   worker = string("worker:", workers)
   gspc_home = string("--gspc-home=", readchomp(`spack location -i gpi-space`))
   nodefile = string("--nodefile=", nodefile)
@@ -9,17 +9,17 @@ function client(workers::Int, nodefile::String, rif_strategy::String, log_host::
   log_port = string("--log-port=", log_port)
   client_config = string(gspc_home, " " ,nodefile, " ",rif_strategy, " ", log_host, " ", log_port, " --worker-env-copy-current")
 
-  DistributedWorkflow.Client(worker, client_config)
+  Client(worker, client_config)
 end
 
-function client(workers::Int, nodefile::String, rif_strategy::String)
+function client_init(workers::Int, nodefile::String, rif_strategy::String)
   worker = string("worker:", workers)
   gspc_home = string("--gspc-home=", readchomp(`spack location -i gpi-space`))
   nodefile = string("--nodefile=", nodefile)
   rif_strategy = string("--rif-strategy=", rif_strategy)
   client_config = string(gspc_home, " " ,nodefile, " ",rif_strategy, " --worker-env-copy-current")
 
-  DistributedWorkflow.Client(worker, client_config)
+  Client(worker, client_config)
 end
 
 function_name(port_name::String, path::String) = KeyValuePair(port_name, path)
