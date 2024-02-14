f6876862cdfc69c33b6f00606ab87e5d5746d5a3
root <firoozehaga@gmail.com>
2024-02-14 18:06:23 +0000

    generic executor and config working

--- examples/executor_files/execute_hello_julia.jl
+++ examples/executor_files/execute_hello_julia.jl
@@ -1,17 +1,15 @@
 # ================================================================
 # hello julia Petri net
 # ================================================================
-# using DistributedWorkflow, CxxWrap
+using DistributedWorkflow, CxxWrap
 
 DistributedWorkflow.compile_workflow("/root/zeda-examples/hello_julia/hello_julia.xpnet", "/root/tmp/build_dir")
 client = DistributedWorkflow.client(1, "/root/tmp/nodefile", "local", "localhost", 6789)
-# impl_path = "julia /root/DistributedWorkflow.jl/utils/executor.jl"
-# impl = DistributedWorkflow.implementation("implementation", impl_path)
 
+impl_port = "implementation"
 jl_impl = "/root/DistributedWorkflow.jl/examples/applications/hello_julia.jl"
-
 fname = "test_func"
-app = DistributedWorkflow.application_config(jl_impl, fname)
+app = DistributedWorkflow.application_config(impl_port, jl_impl, fname)
 out_dir = "/root/tmp/output_dir_hello"
 
 workflow_N2M_jl = DistributedWorkflow.workflow_config("hello_julia.pnet", out_dir, app)
